4860de1a1c04282ec4534679400199fc9a139675
Eric <t105590021@ntut.org.tw>
2020-12-28 23:47:12 +0800

    main

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int score;
+        System.out.print("Enter the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            score = Integer.parseInt(br.readLine());
+            char grade = letterGrade.letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        }
+    }
+}
